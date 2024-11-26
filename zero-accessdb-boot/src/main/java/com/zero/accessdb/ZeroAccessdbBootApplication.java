46ec6ac0e1177e966eb390217a642bd04fbef611
Micro-Organism <coolrabbit1993@gmail.com>
2024-11-26 17:29:59 +0800

    feat: springboot 集成 access-database

---
+++ zero-accessdb-boot/src/main/java/com/zero/accessdb/ZeroAccessdbBootApplication.java
@@ -0,0 +1,13 @@
+package com.zero.accessdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroAccessdbBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroAccessdbBootApplication.class, args);
+    }
+
+}
---
+++ zero-accessdb-boot/src/test/java/com/zero/accessdb/ZeroAccessdbBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.accessdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroAccessdbBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
